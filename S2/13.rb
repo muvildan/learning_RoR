79b2524e04e805bacee211323168d418bd75fbe4
muvildan <danielvilches98@gmail.com>
2024-06-11 10:28:12 +0200

    S2_13

---
+++ S2/13.rb
@@ -0,0 +1,21 @@
+# Say hello world in multiple ways
+# Simple puts string
+# Using a variable
+# Using a method
+
+# Simple puts string
+puts "Hello, World!" # nil after output
+p "Hello, World!" # No nil after output
+print "Hello, World!" # No new line "\n" after output
+
+# Using a variable
+greeting = "Hello, World!" # def the variable
+puts greeting # puts the variable
+
+# Using a method
+def say_hello(thing_to_say) # def the method
+    puts thing_to_say
+end
+
+say_hello "Hello, World!" # calls the method passing a string as an argument
+say_hello(greeting) # calls the method passing a variable as an argument
