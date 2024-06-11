7c44f607d511b8ef2691effbee430e91a4f93131
muvildan <danielvilches98@gmail.com>
2024-06-11 16:42:15 +0200

    S2_calculator

---
+++ S2/calculator.rb
@@ -0,0 +1,13 @@
+# Simple calculator
+puts "Simple calculator"
+20.times {print "-"}
+puts "\n"
+puts "Enter the first  number"
+num_1 = gets.chomp
+puts "Enter the second numbers"
+num_2 = gets.chomp
+puts "The first number plus the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_i}"
+puts "The module of the first number divided by the second number is #{num_1.to_i % num_2.to_i}"
