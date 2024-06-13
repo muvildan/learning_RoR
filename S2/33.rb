2ae2782ea908d2ba2d4e274211a1e464fac1f742
muvildan <danielvilches98@gmail.com>
2024-06-13 10:32:38 +0200

    S2_33

---
+++ S2/33.rb
@@ -0,0 +1,15 @@
+# Styling conventions in Ruby on Rails
+# Snake case
+this_is_snake_case # For method names and variable assignments
+
+def my_name_is
+  # add here your code
+end
+
+# Camel case
+ThisIsCamelCase # For class names
+thisIsAlsoCamelCase # For method names
+
+class SomeClass
+  # add here your code
+end
