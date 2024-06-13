db12b551770b53417c9c1b9d41aad9723205a2ba
muvildan <danielvilches98@gmail.com>
2024-06-13 12:42:55 +0200

    S2_40

---
+++ S2/40.rb
@@ -0,0 +1,20 @@
+# OOP
+
+# Definition
+# ----------
+# Object-Oriented Programming (OOP) is a programming paradigm that uses
+# objects and their interactions to design and program applications.
+
+# University
+# ----------
+# Student
+# -- students - first name, last name, email, student id
+# Course
+# -- courses - course name, course description, course id
+
+# Bookstore
+# ----------
+# Book
+# -- books - ISBN, name, description
+# Employee
+# -- employees - SSN, first name, last name, email, employee id
