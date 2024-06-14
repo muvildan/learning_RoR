5596e64dee06cfac3cefcdb4647ab54dea81ef1f
muvildan <danielvilches98@gmail.com>
2024-06-14 17:24:39 +0200

    S2_OOP_final_project restructuring

--- S2/OOP/final_project/main.rb
+++ S2/OOP/final_project/main.rb
@@ -10,13 +10,13 @@
 require 'bcrypt'
 
 my_password = BCrypt::Password.create("my password")
-
 puts my_password
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
 
-puts my_password = BCrypt::Password.new("$2a$12$NXmFwxHLUVV6KMzADoX1EefXg4U3FHhAYQEcDqBGioXY7VqjvpTe2")
-puts my_password == "my password"     #=> true
+# puts my_password.version              #=> "2a"
+# puts my_password.cost                 #=> 12
+# puts my_password == "my password"     #=> true
+# puts my_password == "not my password" #=> false
+
+my_password = BCrypt::Password.new("$2a$12$GyDoyCebvfrzdDTqB67I8.rdARj6pOQGA.Gpyt2.pAo6cCN1amlBS")
+puts my_password == "my password" #=> true
 puts my_password == "not my password" #=> false
